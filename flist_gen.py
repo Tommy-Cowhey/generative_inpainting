736765383ad750eea0cc1c2b51eaac049e98c4e6
Tommy Cowhey <thomascowhey2023@u.northwestern.edu>
2023-05-12 18:27:17 +0000

    .gitignore update

--- flist_gen.py
+++ flist_gen.py
@@ -1,10 +1,10 @@
 import os
 
-#dir_path = '../ASL-deep-learning/data/train/images'
-#flist_path = './train_flist.txt'
+dir_path = '../train/images'
+flist_path = './train_flist.txt'
 
-dir_path = '../ASL-deep-learning/data/valid/images'
-flist_path = './valid_flist.txt'
+#dir_path = '../valid/images'
+#flist_path = './valid_flist.txt'
 
 with open(flist_path, 'w') as f:
     for filename in os.listdir(dir_path):
